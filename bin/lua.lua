79d87d43e3b2065bde888b76bda3adf07f368a46
Lupus590 <lupussolitarius590@gmail.com>
2020-04-04 20:05:49 +0100

    apparently this is needed too

--- bin/lua.lua
+++ bin/lua.lua
@@ -276,7 +276,10 @@ local packagePreloadBackup = {}
 for k, v in pairs(package.preload) do
   packagePreloadBackup[k] = v
 end
-
+local packageLoadedBackup = {}
+for k, v in pairs(package.loaded) do
+  packageLoadedBackup[k] = v
+end
 
 local environment = setmetatable({
   exit = setmetatable({}, {
@@ -290,6 +293,10 @@ local environment = setmetatable({
       package.preload = {}
       for k, v in pairs(packagePreloadBackup) do
         package.preload[k] = v
+      end
+      package.loaded = {}
+      for k, v in pairs(packageLoadedBackup) do
+        package.loaded[k] = v
       end
     end,
 
