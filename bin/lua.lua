4dbfa4101aea08db09332af33411230e1e39989f
Lupus590 <lupussolitarius590@gmail.com>
2020-04-06 14:19:01 +0100

    requested changes

--- bin/lua.lua
+++ bin/lua.lua
@@ -272,40 +272,6 @@ local history = {}
 local counter = 1
 local output = {}
 
-local autoRunEnv = setmetatable({}, { __index = _ENV })
-
-
-local function processAutoRunFile(folderPath, file)
-  if string.sub( file, 1, 1 ) == "." then return end
-
-  local path = fs.combine(folderPath, file)
-  if fs.isDir( path ) then return end
-
-  local func, err = loadfile(path, nil, autoRunEnv)
-  if not func then
-    printError(path..": "..err)
-    return
-  end
-
-  local ok, result
-  if settings.get("mbs.lua.traceback", true) then
-    ok, result = stack_trace.xpcall_with(func)
-  else
-    ok, result = pcall(func)
-  end
-  if not ok then
-    printError(folderPath.."/"..result)
-  end
-end
-
-local function loadAutoRun(folderPath)
-  if fs.exists( folderPath ) and fs.isDir( folderPath ) then
-    local files = fs.list( folderPath )
-    for _, file in ipairs( files ) do
-      processAutoRunFile(folderPath, file)
-    end
-  end
-end
 
 local environment = setmetatable({
   exit = setmetatable({}, {
@@ -316,10 +282,43 @@ local environment = setmetatable({
   _noTail = function(...) return ... end,
 
   out = output,
-}, { __index = autoRunEnv })
+}, { __index = _ENV })
 
-loadAutoRun("/rom/lua_autorun")
-loadAutoRun("/lua_autorun")
+
+local function process_auto_run_file(folderPath, file)
+  if string.sub( file, 1, 1 ) == "." then return end
+
+  local path = fs.combine(folderPath, file)
+  if fs.isDir( path ) then return end
+
+  local func, err = loadfile(path, nil, _ENV)
+  if not func then
+    printError(err)
+    return
+  end
+
+  local ok, result
+  if settings.get("mbs.lua.traceback", true) then
+    ok, result = stack_trace.xpcall_with(func)
+  else
+    ok, result = pcall(func)
+  end
+  if not ok then
+    printError(result)
+  end
+end
+
+local function load_auto_run_folder(folderPath)
+  if fs.exists( folderPath ) and fs.isDir( folderPath ) then
+    local files = fs.list( folderPath )
+    for _, file in ipairs( files ) do
+      process_auto_run_file(folderPath, file)
+    end
+  end
+end
+
+load_auto_run_folder("/rom/lua_autorun")
+load_auto_run_folder("/lua_autorun")
 
 local autocomplete = nil
 if not settings or settings.get("lua.autocomplete") then
