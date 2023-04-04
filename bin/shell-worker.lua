277f49b03d1047511cf860d87a2f54b03386a568
red <lukas.fricke.dev@gmail.com>
2023-04-04 12:56:27 +0200

    Steeled even moor code from squid

--- bin/shell-worker.lua
+++ bin/shell-worker.lua
@@ -48,7 +48,7 @@ local function get_first_startup()
   if fs.exists("startup.lua") and not fs.isDir("startup.lua") then
     return "startup.lua"
   end
-  if fs.isDir("startup")  then
+  if fs.isDir("startup") then
     local first = fs.list("startup")[1]
     if first then return fs.combine("startup", first) end
   end
@@ -62,6 +62,75 @@ local function show_prompt()
   if term.getCursorPos() ~= 1 then print() end
   write(shell.dir() .. "> ")
   term.setTextColour(textColour)
+end
+
+-- This import function is stolen from CC-Tweaked (cc.internal.import)
+local completion = require "cc.completion"
+
+local function import(files)
+  local overwrite = {}
+  for _, file in pairs(files) do
+    local filename = file.getName()
+    local path = shell.resolve(filename)
+    if fs.exists(path) then
+      if fs.isDir(path) then
+        return nil, filename .. " is already a directory."
+      end
+
+      overwrite[#overwrite + 1] = filename
+    end
+  end
+
+  if #overwrite > 0 then
+    table.sort(overwrite)
+    printError("The following files will be overwritten:")
+    textutils.pagedTabulate(colours.cyan, overwrite)
+
+    while true do
+      io.write("Overwrite? (yes/no) ")
+      local input = read(nil, nil, function(t)
+        return completion.choice(t, { "yes", "no" })
+      end)
+      if not input then return end
+
+      input = input:lower()
+      if input == "" or input == "yes" or input == "y" then
+        break
+      elseif input == "no" or input == "n" then
+        return
+      end
+    end
+  end
+
+  for _, file in pairs(files) do
+    local filename = file.getName()
+    print("Transferring " .. filename)
+
+    local path = shell.resolve(filename)
+    local handle, err = fs.open(path, "wb")
+    if not handle then return nil, err end
+
+    -- Write the file without loading it all into memory. This uses the same buffer size
+    -- as BinaryReadHandle. It would be really nice to have a way to do this without
+    -- multiple copies.
+    while true do
+      local chunk = file.read(8192)
+      if not chunk then break end
+
+      local ok, err = pcall(handle.write, chunk)
+      if not ok then
+        handle.close()
+
+        -- Probably an out-of-space issue, just bail.
+        if err:sub(1, 7) == "pcall: " then err = err:sub(8) end
+        return nil, "Failed to write file (" .. err .. "). File may be corrupted"
+      end
+    end
+
+    handle.close()
+  end
+
+  return true
 end
 
 local scroll_offset = 0
@@ -160,7 +229,7 @@ while coroutine.status(worker) ~= "dead" do
     term.setCursorBlink(false)
 
     -- Run the import script with the provided files
-    local ok, err = require("cc.internal.import")(event[2].getFiles())
+    local ok, err = import(event[2].getFiles())
     if not ok and err then printError(err) end
 
     -- And attempt to restore the prompt.
